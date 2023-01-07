35bc2de70fc809f332814a1b73aec8471e353f60
Gizru <48158405+Gizru@users.noreply.github.com>
2023-01-07 15:27:41 +0100

    Add project files.

---
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -0,0 +1,75 @@
+﻿using P3AddNewFunctionalityDotNetCore.Controllers;
+using P3AddNewFunctionalityDotNetCore.Models;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System.Collections.Generic;
+using Xunit;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests
+{
+    public class ProductServiceTests
+    {
+        /// <summary>
+        /// Take this test method as a template to write your test method.
+        /// A test method must check if a definite method does its job:
+        /// returns an expected value from a particular set of parameters
+        /// </summary>
+        [Fact]
+        public void CheckProduct()
+        {
+            Cart cart = new Cart();
+
+            ProductController productController = new ProductController();
+            ProductViewModel product = new ProductViewModel();
+            List<string> errorMessages = productController.CheckProduct(product);
+
+            List<string> expectedErrorMessages = new List<string> {
+                "MissingName",
+                "MissingQuantity",
+                "QuantityNotGreaterThanZero",
+                "QuantityNotAnInteger",
+                "MissingPrice",
+                "PriceNotGreaterThanZero",
+                "PriceNotANumber"
+            };
+
+            Assert.NotEmpty(errorMessages);
+            Assert.Contains("MissingName",errorMessages);
+            Assert.Contains("MissingQuantity", errorMessages);
+            Assert.Contains("MissingPrice", errorMessages);
+
+
+            //we modify the condition to check other error messages
+            product.Price = "not a number";
+            product.Stock = "10.5";
+            errorMessages = productController.CheckProduct(product);
+
+
+            Assert.Contains("MissingName", errorMessages);
+            Assert.Contains("QuantityNotAnInteger", errorMessages);
+            Assert.Contains("PriceNotANumber", errorMessages);
+
+
+            //we modify the condition to check other error messages
+            product.Price = "-10.5";
+            product.Stock = "-5";
+            errorMessages = productController.CheckProduct(product);
+
+
+            Assert.Contains("MissingName", errorMessages);
+            Assert.Contains("PriceNotGreaterThanZero", errorMessages);
+            Assert.Contains("QuantityNotGreaterThanZero", errorMessages);
+
+
+            //checking that we receive no error with the right conditions
+            product.Name = "Test";
+            product.Price = "10.5";
+            product.Stock = "5";
+            errorMessages = productController.CheckProduct(product);
+
+            Assert.Empty(errorMessages);
+        }
+
+        // TODO write test methods to ensure a correct coverage of all possibilities
+    }
+}
---
+++ P3AddNewFunctionalityDotNetCore/Controllers/ProductController.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P3AddNewFunctionalityDotNetCore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ILanguageService _languageService;
+
+        public ProductController(IProductService productService, ILanguageService languageService)
+        {
+            _productService = productService;
+            _languageService = languageService;
+        }
+
+
+        //This contructor is used for TESTING ONLY
+        //Do not use it otherwise
+        public ProductController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ProductViewModel> products = _productService.GetAllProductsViewModel();
+            return View(products);
+        }
+
+        [Authorize]
+        public IActionResult Admin()
+        {
+            return View(_productService.GetAllProductsViewModel().OrderByDescending(p => p.Id));
+        }
+
+        [Authorize]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        public List<string> CheckProduct(ProductViewModel product)
+        {
+            List<string> modelErrors = new List<string>();
+
+            if(product.Name is null)
+            {
+                modelErrors.Add("MissingName");
+            }
+
+            try
+            {
+                int lStock = Int32.Parse(product.Stock);
+
+                if (lStock <= 0)
+                {
+                    modelErrors.Add("QuantityNotGreaterThanZero");
+                }
+            }
+            catch (ArgumentNullException error)
+            {
+                modelErrors.Add("MissingQuantity");
+            }
+            catch (FormatException error)
+            {
+                modelErrors.Add("QuantityNotAnInteger");
+            }
+
+            try
+            {
+                float lPrice = float.Parse(product.Price);
+
+                if (lPrice <= 0)
+                {
+                    modelErrors.Add("PriceNotGreaterThanZero");
+                }
+            }
+            catch (ArgumentNullException error)
+            {
+                modelErrors.Add("MissingPrice");
+            }
+            catch (FormatException error)
+            {
+                modelErrors.Add("PriceNotANumber");
+            }
+
+            return modelErrors;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Create(ProductViewModel product)
+        {
+            List<string> modelErrors = CheckProduct(product);
+            // TODO validation controls
+            // Implement a method inside the ProductService class that will return an error message for each
+            // product property that is not conform to its business rules. The return type of the method 
+            // must be of List<string>.
+
+            foreach (string error in modelErrors)
+            {
+                ModelState.AddModelError("", error);
+            }
+
+            if (ModelState.IsValid)
+            {
+                _productService.SaveProduct(product);
+                return RedirectToAction("Admin");
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult DeleteProduct(int id)
+        {
+            _productService.DeleteProduct(id);
+            return RedirectToAction("Admin");
+        }
+    }
+}
